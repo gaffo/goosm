d423d8ec996889b74541b5763ac353c7d8e21692
Mike Gaffney <mr.gaffo@gmail.com>
2015-06-25 17:47:57 -0700

    Added Encoding

--- osm.go
+++ osm.go
@@ -1,65 +1,10 @@
-package hunt
+package goosm
 
 import (
 	"encoding/xml"
 	"io/ioutil"
+	"io"
 )
-
-// Decimal 	0 	1 	2 	3 	4 	5 	6 	7 	8 	9 	10 	11 	12 	13 	14 	15
-// Base 32 	0 	1 	2 	3 	4 	5 	6 	7 	8 	9 	b 	c 	d 	e 	f 	g
-//
-// Decimal 	16 	17 	18 	19 	20 	21 	22 	23 	24 	25 	26 	27 	28 	29 	30 	31
-// Base 32 	h 	j 	k 	m 	n 	p 	q 	r 	s 	t 	u 	v 	w 	x 	y 	z
-
-var geoHashDict = map[string]int{
-	"0": 0,
-	"1": 1,
-	"2": 2,
-	"3": 3,
-	"4": 4,
-	"5": 5,
-	"6": 6,
-	"7": 7,
-	"8": 8,
-	"9": 9,
-	"b": 10,
-	"c": 11,
-	"d": 12,
-	"e": 13,
-	"f": 14,
-	"g": 15,
-	"h": 16,
-	"j": 17,
-	"k": 18,
-	"m": 19,
-	"n": 20,
-	"p": 21,
-	"q": 22,
-	"r": 23,
-	"s": 24,
-	"t": 25,
-	"u": 26,
-	"v": 27,
-	"w": 28,
-	"x": 29,
-	"y": 30,
-	"z": 31,
-}
-
-type LL struct {
-	Lat float64
-	Lon float64
-}
-
-func (ll LL) GeoHash() string {
-	return ""
-}
-
-type GeoHash string
-
-func (geohash GeoHash) LL() LL {
-	return LL{}
-}
 
 type OsmChange struct {
 	Creates   []Create `xml:"create"`
@@ -79,6 +24,24 @@ type Osm struct {
 	Version   string     `xml:"version,attr"`
 	Upload    bool       `xml:"upload,attr"`
 	Generator string     `xml:"generator,attr"`
+	XMLName struct{}	`xml:"osm"`
+}
+
+func NewOsm() *Osm {
+	return &Osm{
+		Version: "0.6",
+		Generator: "github.com/gaffo/goosm",
+		Upload: true,
+		Nodes: make([]Node, 0, 128),
+		Ways: make([]Way, 0, 128),
+		Relations: make([]Relation, 0, 128),
+	}
+}
+
+func (osm *Osm) Write(writer io.Writer) {
+	e := xml.NewEncoder(writer)
+	e.Encode(osm)
+	e.Flush()
 }
 
 type Node struct {
--- osm_test.go
+++ osm_test.go
@@ -1,26 +1,67 @@
-package hunt
+package goosm_test
 
 import (
 	"testing"
+	"bytes"
+	"runtime"
+	"fmt"
+	"path/filepath"
+	"reflect"
+	"github.com/gaffo/goosm"
 )
 
-// func TestGeohashFromLL(t *testing.T) {
-// 	equals(t, "u4pruydqqvj", LL{57.64911, 10.40744}.GeoHash())
-// }
+func fail(tb testing.TB, reason string) {
+	_, file, line, _ := runtime.Caller(1)
+	fmt.Printf("%s:%d: %s\n\n", filepath.Base(file), line, reason)
+	tb.FailNow()
+}
 
-// func TestLLFromGeohash(t *testing.T) {
-// 	equals(t, LL{57.64911, 10.40744}, GeoHash("u4pruydqqvj").LL())
-// }
+func contains(tb testing.TB, s []string, e string) {
+	for _, a := range s {
+		if a == e {
+			return
+		}
+	}
+	fmt.Printf("Expected to contain %s\n", e)
+	tb.FailNow()
+}
+
+// assert fails the test if the condition is false.
+func assert(tb testing.TB, condition bool, msg string, v ...interface{}) {
+	if !condition {
+		_, file, line, _ := runtime.Caller(1)
+		fmt.Printf("%s:%d: "+msg+"\n\n", append([]interface{}{filepath.Base(file), line}, v...)...)
+		tb.FailNow()
+	}
+}
+
+// ok fails the test if an err is not nil.
+func ok(tb testing.TB, err error) {
+	if err != nil {
+		_, file, line, _ := runtime.Caller(1)
+		fmt.Printf("%s:%d: unexpected error: %s\n\n", filepath.Base(file), line, err.Error())
+		tb.FailNow()
+	}
+}
+
+// equals fails the test if exp is not equal to act.
+func equals(tb testing.TB, exp, act interface{}) {
+	if !reflect.DeepEqual(exp, act) {
+		_, file, line, _ := runtime.Caller(1)
+		fmt.Printf("%s:%d:\n\n\texp: %#v\n\n\tgot: %#v\n\n", filepath.Base(file), line, exp, act)
+		tb.FailNow()
+	}
+}
 
 func TestParseChangeEmpty(t *testing.T) {
-	osm := ParseChange("blilbirp")
+	osm := goosm.ParseChange("blilbirp")
 	if osm != nil {
 		fail(t, "not nil")
 	}
 }
 
 func TestParseChange(t *testing.T) {
-	osm := ParseChange("test/osm_change.xml")
+	osm := goosm.ParseChange("test/osm_change.xml")
 	if osm == nil {
 		fail(t, "nil")
 	}
@@ -32,7 +73,7 @@ func TestParseChange(t *testing.T) {
 }
 
 func TestParseOregon(t *testing.T) {
-	osm := ParseOsm("test/or.osm")
+	osm := goosm.ParseOsm("test/or.osm")
 	if osm == nil {
 		t.FailNow()
 	}
@@ -64,7 +105,7 @@ func TestParseOregon(t *testing.T) {
 }
 
 func TestParseWashington(t *testing.T) {
-	osm := ParseOsm("test/wa.osm")
+	osm := goosm.ParseOsm("test/wa.osm")
 	if osm == nil {
 		t.FailNow()
 	}
@@ -107,8 +148,25 @@ func TestParseWashington(t *testing.T) {
 }
 
 func TestParseNonexistent(t *testing.T) {
-	osm := ParseOsm("blibbity")
+	osm := goosm.ParseOsm("blibbity")
 	if osm != nil {
 		t.FailNow()
 	}
+}
+
+func TestEncode(t *testing.T) {
+	var b bytes.Buffer
+
+	osm := goosm.NewOsm()
+	osm.Write(&b)
+	equals(t, `<osm version="0.6" upload="true" generator="github.com/gaffo/goosm"></osm>`, b.String())
+}
+
+func TestEncodeWithNode(t *testing.T) {
+	var b bytes.Buffer
+	osm := goosm.NewOsm()
+	osm.Nodes = append(osm.Nodes, goosm.Node{Id: "-1", Visible: true, Lat: 30.1, Lon: -122.33})
+	osm.Write(&b)
+	fmt.Println(b.String())
+	equals(t, `<osm version="0.6" upload="true" generator="github.com/gaffo/goosm"><node id="-1" visible="true" lat="30.1" lon="-122.33"></node></osm>`, b.String())
 }
